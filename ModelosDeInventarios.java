6d1fdf1afd1a203259460db0759871bd04af7375
RoiSol19 <roy.gonzr20@gmail.com>
2025-11-07 21:05:20 -0600

    Ingresa tus datos

---
+++ ModelosDeInventarios.java
@@ -0,0 +1,11 @@
+Java.util.Scanner;
+public class ModelosDeInvetarios {
+    public static void main(String[] args) {
+        Scanner Sol=new Scanner(System.in);
+        System.in.println("Ingresa tu demanda: ");
+        int demanda=Sol.nextInt();
+        System.in.println("Ingresa tu precio pedido: ")
+        pedido=Sol.nextInt();
+    }
+}
+
