5069683b571418b39a721bd168955e03482c5be4
RoiSol19 <roy.gonzr20@gmail.com>
2025-11-09 21:26:43 -0600

    Primera formula

--- ModelosDeInventarios.java
+++ ModelosDeInventarios.java
@@ -2,10 +2,16 @@ Java.util.Scanner;
 public class ModelosDeInvetarios {
     public static void main(String[] args) {
         Scanner Sol=new Scanner(System.in);
-        System.in.println("Ingresa tu demanda: ");
+        System.in.println("Ingresa tu demanda (D): ");
         int demanda=Sol.nextInt();
-        System.in.println("Ingresa tu precio pedido: ")
+        System.in.println("Ingresa tu precio pedido (K): ");
         pedido=Sol.nextInt();
+        System.out.printn("Ingresa tu costo de almacenamiento (H): ");
+        h=Sol.nextInt();
+        System.out.println("Ingrese los dias en el que recibira el producto (L): ")
+        L=Sol.nextInt();
+        double y*=Math.sqrt((2*demanda*pedido)/h);
+        System.out.println("y* = " + y*)
     }
 }
 
