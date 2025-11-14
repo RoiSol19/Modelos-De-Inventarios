8499801b47503acbfcda434761f526aac3c0342e
RoiSol19 <roy.gonzr20@gmail.com>
2025-11-13 19:54:18 -0600

    Correcciones y punto de reorden (Le)

--- ModelosDeInventarios.java
+++ ModelosDeInventarios.java
@@ -1,18 +1,20 @@
-Java.util.Scanner;
+import Java.util.Scanner;
 public class ModelosDeInvetarios {
     public static void main(String[] args) {
         Scanner Sol=new Scanner(System.in);
-        System.in.println("Ingresa tu demanda (D): ");
-        double demanda=Sol.nextInt();
-        System.in.println("Ingresa tu precio pedido (K): ");
-        double pedido=Sol.nextInt();
+        System.out.println("Ingresa tu demanda (D): ");
+        double D=Sol.nextInt();
+        System.out.println("Ingresa tu precio pedido (K): ");
+        double K=Sol.nextInt();
         System.out.printn("Ingresa tu costo de almacenamiento (H): ");
         double h=Sol.nextInt();
-        System.out.println("Ingrese los dias en el que recibira el producto (L): ")
+        System.out.println("Ingrese los dias en el que recibira el producto (L): ");
         int L=Sol.nextInt();
-        double y*=Math.sqrt((2*demanda*pedido)/h);
-        double to=(y*/demanda);
-        System.out.println("La cantidad del pedido es (y*): " + y*);
+        double y=Math.sqrt((2*D*K)/h);
+        double to=(y/D);
+        double n=L/to;
+        double Le=L-(n*to);
+        System.out.println("La cantidad del pedido es (y*): " + y);
         System.out.println("La duracion del ciclo (to): " + (to));
     }
 }
