9482f0e664a2457ea7382cdf90d440c478dde902
RoiSol19 <roy.gonzr20@gmail.com>
2025-11-13 22:06:32 -0600

    Modelo de inventarios con descuentoy correccion de errores

--- ModelosDeInventarios.java
+++ ModelosDeInventarios.java
@@ -1,37 +1,66 @@
 import Java.util.Scanner;
 public class ModelosDeInvetarios {
     public static void main(String[] args) {
+        Scanner Sol=new Scanner(System.in);
+        int opcion=0;
+        int Opcion=0;
         do{
             System.out.println("\n \n \tMenu");
             System.out.println("1. Modelo de Ineventarios basico");
             System.out.println("2. Modelo de invenatrios con descuento");
             System.out.println("Salir");
-            Syste.out.println("Opcion: ");
+            System.out.println("Opcion: ");
             opcion=Sol.nextInt();
             switch(opcion){
                 case 1:
-
+                    System.out.println("Ingresa tu demanda (D): ");
+                    double D=Sol.nextDouble();
+                    System.out.println("Ingresa tu precio pedido (K): ");
+                    double K=Sol.nextDouble();
+                    System.out.println("Ingresa tu costo de almacenamiento (H): ");
+                    double h=Sol.nextDouble();
+                    System.out.println("Ingrese los dias en el que recibira el producto (L): ");
+                    int L=Sol.nextDouble();
+                    double y=Math.sqrt((2*D*K)/h);
+                    double to=(y/D);
+                    double n=L/to;
+                    double Lc=L-(n*to);
+                    double Le=D*Lc;
+                    double CTU=(K/y/D)+(h*y/2);
+                    System.out.println("La cantidad del pedido es (y*): " + y);
+                    System.out.println("La duracion del ciclo (to): " + to);
+                    System.out.println("El pedido de reorden es (Le): " + Le);
+                    System.out.println("El Costo Total Unitario es (CTU): " + CTU);
+                    break;
+                case 2:
+                    System.out.println("Ingresa tu demanda (D): ");
+                    double d=Sol.nextDouble();
+                    System.out.println("Ingresa tu precio pedido (K): ");
+                    double k=Sol.nextInt();
+                    System.out.println("Ingresa la cantidad de costos que tiene tu modelo de inventarios: ");
+                    Opcion=Sol.nextInt();
+                    switch(Opcion){
+                        case 1:
+                            System.out.println("Ingresa el costo unitario (C): ");
+                            double c=Sol.nextDouble();
+                            break;
+                        case 2:
+                            System.out.println("Ingresa el primer costo unitario (C): ");
+                            double c1=Sol.nextDouble();
+                            System.out.println("Ingresa el segundo costo unitario (C): ");
+                            double c2=Sol.nextDouble();
+                            break;
+                        case 3:
+                            System.out.println("Ingresa el primer costo unitario (C): ");
+                            double c1=Sol.nextDouble();
+                            System.out.println("Ingresa el segundo costo unitario (C): ");
+                            double c2=Sol.nextDouble();
+                            System.out.println("Ingresa el tercer costo unitario (C): ");
+                            double c3=Sol.nextDouble();
+                            break;
+                    }
             }
         }while(opcion!=3);
-        Scanner Sol=new Scanner(System.in);
-        System.out.println("Ingresa tu demanda (D): ");
-        double D=Sol.nextInt();
-        System.out.println("Ingresa tu precio pedido (K): ");
-        double K=Sol.nextInt();
-        System.out.printn("Ingresa tu costo de almacenamiento (H): ");
-        double h=Sol.nextInt();
-        System.out.println("Ingrese los dias en el que recibira el producto (L): ");
-        int L=Sol.nextInt();
-        double y=Math.sqrt((2*D*K)/h);
-        double to=(y/D);
-        double n=L/to;
-        double Lc=L-(n*to);
-        double Le=D*Lc;
-        double CTU=(k/y/D)+(H*Y/2);
-        System.out.println("La cantidad del pedido es (y*): " + y);
-        System.out.println("La duracion del ciclo (to): " + to);
-        System.out.println("El pedido de reorden es (Le): " + Le);
-        System.out.println("El Costo Total Unitario es (CTU): " + CTU);
     }
 }
 
